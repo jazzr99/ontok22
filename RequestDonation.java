9356b348c29fa72f9fcce099ee3cd87810bc3ab0
jazzr99 <106020625+jazzr99@users.noreply.github.com>
2022-05-22 17:56:41 +0300

    Add files via upload

---
+++ RequestDonation.java
@@ -0,0 +1,41 @@
+import java.util.*;  
+
+class RequestDonation 
+{
+    
+    private Entity entity;
+    private double quantity;
+
+    public RequestDonation (){ }
+    
+    public RequestDonation(Entity entity, double quantity){
+     this.entity = entity;    
+     this.quantity = quantity;
+    }
+
+    public boolean equals(Entity entity) {
+        if (this.getEntity().getId() == entity.getId()){
+            return true;
+        }
+     return false;
+    }
+
+    public Entity getEntity() {
+        return entity;
+    }
+
+    public void setEntity(Entity entity) {
+        this.entity = entity;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDetails()
+    {return ("RequestDonation:Entity:"+entity+ " Quantity:"+quantity);}    
+}
