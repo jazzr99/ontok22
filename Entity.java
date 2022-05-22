f5db0410013dc864bb653ec79d8bf9f4322789c1
jazzr99 <106020625+jazzr99@users.noreply.github.com>
2022-05-22 17:54:22 +0300

    Add files via upload

---
+++ Entity.java
@@ -0,0 +1,49 @@
+public abstract class Entity {
+
+    private String name;
+    private String description;
+    private int id;
+
+    public Entity() {
+    }
+
+    public Entity(String name, String description, int id){
+        this.name = name;
+        this.description = description;
+        this.id = id;
+    }
+
+    public void setName(String Name){
+    this.name = Name;
+    }
+
+    public String getName(){
+    return name;
+    }
+
+    public void setDescription(String Description){
+    this.description = Description;
+    }
+
+    public String getDescription(){
+    return description;
+    }
+
+    public void setId(int Id){
+    this.id = Id;
+    }
+
+    public int getId(){
+    return id;
+    }
+
+    public String getEntityInfo(){
+        return("This donation is : " + this.name + " \n Description: " + this.description + " \n id: " + this.id + "." );
+    }
+
+    public abstract String getDetails();
+
+    @Override
+    public String toString() {
+        return getEntityInfo() + getDetails();
+    }}
