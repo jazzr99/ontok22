1c59a2eb18fa4bddb895b9af6da4ba41f42089f9
jazzr99 <106020625+jazzr99@users.noreply.github.com>
2022-05-22 17:53:03 +0300

    Add files via upload

---
+++ Admin.java
@@ -0,0 +1,18 @@
+public class Admin extends User{
+      
+    private boolean isAdmin = true;
+    
+    public Admin(){}
+
+    public Admin(String name, String phone) {
+        super(name, phone);
+    }
+
+    public boolean getAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+}
