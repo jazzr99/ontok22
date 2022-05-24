bf0815a3efad3426ad085d81e11039930904693b
ExonautX <106067621+ExonautX@users.noreply.github.com>
2022-05-24 14:01:38 +0300

    Update RequestDonationList.java

--- RequestDonationList.java
+++ RequestDonationList.java
@@ -3,61 +3,51 @@ import java.util.ArrayList;
 
 public class RequestDonationList extends RequestDonation
 {  
-    private List<RequestDonation> rdEntities = new ArrayList<>();
+    private List<RequestDonation> rdEntities=new ArrayList<>();
     public RequestDonation get(int id)
     {    
-       RequestDonation requestDonation = new RequestDonation();
+       RequestDonation requestDonation=new RequestDonation();
        for (RequestDonation rd:rdEntities){
-           if (rd.getEntity().getId() == id){
-               requestDonation = rd;
+           if (rd.getEntity().getId()==id){
+               requestDonation=rd;
            }}
         return requestDonation;
     }      
     public void add(RequestDonation requestDonation, List<Entity> entityList)
     {
         if(!entityList.contains(requestDonation.getEntity())) {
-            throw new NoEntityFoundException("There is no such entity in  organization's entityList");
+            throw new NoEntityFoundException("This entity does not exist in the organization!");
         }
-
         for (RequestDonation donation:rdEntities){
                 if(requestDonation.equals(donation.getEntity())){
                     donation.setQuantity(donation.getQuantity() + requestDonation.getQuantity());
                 }
-                else{
+                 else{
                     rdEntities.add(requestDonation);
-                }
-        }
-    }
-
-    public List<RequestDonation> getRdEntities() {
+                }}}
+    public List<RequestDonation> getRdEntities(){
         return rdEntities;
     }
-
-    public void setRdEntities(List<RequestDonation> rdEntities) {
-        this.rdEntities = rdEntities;
+    public void setRdEntities(List<RequestDonation> rdEntities){
+        this.rdEntities=rdEntities;
     }
-
     public void remove(RequestDonation requestDonation)
     {
         rdEntities.remove(requestDonation);
     }
-
     public double modify(RequestDonation requestDonation)
     {
         return requestDonation.getQuantity();
     }
-
     public String monitor()
     {
-        for (int i=0; i<rdEntities.size(); i++)
+        for (int x=0; x<rdEntities.size(); x++)
     {
-        return rdEntities.get(i).getDetails();
+        return rdEntities.get(x).getDetails();
     }
-        return " ";
+        return "  ";
     }
-
     public void reset()
     {
         rdEntities.removeAll(rdEntities);
-    }
-}
+    }}
