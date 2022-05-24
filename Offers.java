14c7f75a6ef830025f6a9a460e5b363316c12fe1
ExonautX <106067621+ExonautX@users.noreply.github.com>
2022-05-24 13:44:42 +0300

    Update Offers.java

--- Offers.java
+++ Offers.java
@@ -2,13 +2,9 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Offers extends RequestDonationList {
-
-
     public void commit(RequestDonationList currentDonations, List<Entity> entityList){
         for(RequestDonation requestDonation: currentDonations.getRdEntities()){
             currentDonations.add(requestDonation, entityList);
-        }
-
+         }
         currentDonations.getRdEntities().removeAll(currentDonations.getRdEntities());
-    }
-}
+    }}
