98ed2edd59d74f87b00612fd92a7df49ad697377
karida23 <106026802+karida23@users.noreply.github.com>
2022-05-24 14:33:51 +0300

    Update Requests.java

--- Requests.java
+++ Requests.java
@@ -28,7 +28,7 @@ public class Requests extends RequestDonationList {
                 level=((Material) requestDonation.getEntity()).getLevel3();
             }
             else{
-                System.out.println("Thiw is not acceptable." /n "Please try again.");
+                System.out.println("This is not acceptable." /n "Please try again.");
             }
         }
         for(RequestDonation receivedMaterial : beneficiary.getReceivedList().getRdEntities()){
