532c8a878c16bc8b9f47d6b179ee9b1845da4cd2
ExonautX <106067621+ExonautX@users.noreply.github.com>
2022-05-24 15:11:04 +0300

    Update User.java

--- User.java
+++ User.java
@@ -1,29 +1,21 @@
 public abstract class User {
-
     private String name;
     private String phone;
 
-
     public User(){ }
-
     public User(String name, String phone){
      this.name=new String(name);
      this.phone=new String(phone);
     }
-
     public String getPhone() {
         return phone;
     }
-
     public void setPhone(String phone) {
-        this.phone = phone;
+        this.phone=phone;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
-        this.name = name;
-    }
-}
+        this.name=name;
+    }}
