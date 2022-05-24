ec77bf0a9b21bdb4c468ada06c57e99da0f86614
ExonautX <106067621+ExonautX@users.noreply.github.com>
2022-05-24 12:40:19 +0300

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,18 +1,12 @@
-public class Admin extends User{
-      
+public class Admin extends User{  
     private boolean isAdmin = true;
-    
     public Admin(){}
-
-    public Admin(String name, String phone) {
+    public Admin(String name, String phone){
         super(name, phone);
     }
-
-    public boolean getAdmin() {
+    public boolean getAdmin(){
         return isAdmin;
     }
-
-    public void setAdmin(boolean admin) {
+    public void setAdmin(boolean admin){
         isAdmin = admin;
-    }
-}
+    }}
