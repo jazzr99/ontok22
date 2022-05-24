05b01169b2c2ff42dd7a05656c152c9d5d16d72e
ExonautX <106067621+ExonautX@users.noreply.github.com>
2022-05-24 12:17:24 +0300

    Update Entity.java

--- Entity.java
+++ Entity.java
@@ -1,26 +1,20 @@
 public abstract class Entity {
-
     private String name;
     private String description;
     private int id;
-
     public Entity() {
     }
-
     public Entity(String name, String description, int id){
         this.name = name;
         this.description = description;
         this.id = id;
     }
-
     public void setName(String Name){
     this.name = Name;
     }
-
     public String getName(){
     return name;
     }
-
     public void setDescription(String Description){
     this.description = Description;
     }
@@ -28,19 +22,15 @@ public abstract class Entity {
     public String getDescription(){
     return description;
     }
-
     public void setId(int Id){
     this.id = Id;
     }
-
     public int getId(){
     return id;
     }
-
     public String getEntityInfo(){
-        return("This donation is : " + this.name + " \n Description: " + this.description + " \n id: " + this.id + "." );
+        return(" The current donation is: " + this.name + " \n Description: " + this.description + " \n id: " + this.id + "." );
     }
-
     public abstract String getDetails();
 
     @Override
