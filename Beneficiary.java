19f1f851783c98920faaa06b3c41b7887f849f50
ExonautX <106067621+ExonautX@users.noreply.github.com>
2022-05-24 13:31:02 +0300

    Update Beneficiary.java

--- Beneficiary.java
+++ Beneficiary.java
@@ -2,40 +2,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Beneficiary extends User{
-
     private int noPersons = 1;
     private RequestDonationList receivedList;
     private Requests requestsList;
-
     public Beneficiary(){}
 
-    public Beneficiary(String name, String phone, int noPersons) {
+    public Beneficiary(String name, String phone, int noPersons){
         super(name, phone);
         this.noPersons = noPersons;
     }
-
-    public int getNoPersons() {
+    public int getNoPersons(){
         return noPersons;
     }
-
-    public void setNoPersons(int noPersons) {
+    public void setNoPersons(int noPersons){
         this.noPersons = noPersons;
     }
-
-    public RequestDonationList getReceivedList() {
+    public RequestDonationList getReceivedList(){
         return receivedList;
     }
-
-    public void setReceivedList(RequestDonationList receivedList) {
+    public void setReceivedList(RequestDonationList receivedList){
         this.receivedList = receivedList;
     }
-
-    public Requests getRequestsList() {
+    public Requests getRequestsList(){
         return requestsList;
     }
-
-    public void setRequestsList(Requests requestsList) {
+    public void setRequestsList(Requests requestsList){
         this.requestsList = requestsList;
-    }
-}
-
+    }}
