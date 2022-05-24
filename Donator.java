94f46eca5e71aa8cdaac815be2493cb3a7ab1419
karida23 <106026802+karida23@users.noreply.github.com>
2022-05-24 13:28:59 +0300

    Update Donator.java

--- Donator.java
+++ Donator.java
@@ -1,17 +1,13 @@
 public class Donator extends User{
     private Offers offersList;
-
-    public Donator(String name, String phone) {
+    public Donator(String name, String phone){
         super(name, phone);
     }
-
     public void setOffersList(Offers offersList){
     this.offersList = offersList;
     }
-    
     public Offers getOffersList(){
     return this.offersList;
     }
-    
     public Donator(){}
 }
