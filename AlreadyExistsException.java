26a431669b570a3b6445054e888588f6f9ede309
ExonautX <106067621+ExonautX@users.noreply.github.com>
2022-05-24 13:04:54 +0300

    Update and rename AlreadyExistsException.java to ExistedException.java

--- AlreadyExistsException.java
+++
@@ -1,5 +0,0 @@
-public class AlreadyExistsException extends RuntimeException{
-    public AlreadyExistsException(String message) {
-        super(message);
-    }
-}
---
+++ ExistedException.java
@@ -0,0 +1,4 @@
+public class ExistedException extends RuntimeException{
+    public ExistedException(String message){
+        super(message);
+    }}
